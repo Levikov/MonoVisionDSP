86a6f9b00b191f841a79e104baf55185f1605f94
Victor LI <victor-li@outlook.com>
2017-12-25 17:22:45 +0800

    Image segment process initialization

--- initImgSegProc.c
+++ initImgSegProc.c
@@ -2,5 +2,13 @@
 
 void initImgSegProc()
 {
-    
+    Error_Block eb;
+    Error_init(&eb);
+    Clock_Params_init(&clockParams[DNUM]);
+    clockParams[DNUM].period = 2;
+    clockParams[DNUM].arg = (UArg)DNUM;
+    clockParams[DNUM].startFlag = TRUE;
+    clockHandle[DNUM] = Clock_create(taskImageSegment,2,&clockParams[DNUM],&eb);
+    Clock_start(clockHandle[DNUM]);
+
 }
