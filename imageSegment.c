2cb479fb1a28c0cc2b3b8c44671f5aad04d03758
Victor LI <victor-li@outlook.com>
2017-12-25 18:33:05 +0800

    Fixed cache incoherency bug.

--- imageSegment.c
+++ imageSegment.c
@@ -1,8 +1,20 @@
 #include "MonoGlobal.h"
 #include <xdc/std.h>
 
-Void taskImageSegment()
+Void taskImageSegment(UArg a0)
 {
-    
+        #ifdef DEBUG
+        //log processing time
+        unsigned int t_start = Timestamp_get32();
+        #endif
+        Cache_inv((xdc_Ptr)&(inputBuffer.headId),4,Cache_Type_ALLD,1);
+        if(inputBuffer.tailId<inputBuffer.headId-1)
+        inputBuffer.tailId++;
+        Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L1D,1);
+        Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L2D,1);
 
+        #ifdef DEBUG
+        debug_imageSegment_time = (float)(Timestamp_get32() - t_start)/1000;
+        #endif
+        
 }
--- receiveNewImage.c
+++ receiveNewImage.c
@@ -3,18 +3,20 @@
 
 Void taskReceiveNewImage(UArg a0)
 {
+        //Cache_inv((xdc_Ptr)&inputBuffer,sizeof(inputBuffer),Cache_Type_ALL,1);
         #ifdef DEBUG
         //log processing time
         unsigned int t_start = Timestamp_get32();
-
         #endif
         //Copy debug image into buffer;
         memcpy(&inputBuffer.inputFrames[inputBuffer.headId%IMG_BUFFER_SIZE].buffer,debug_img,IMG_SIZE);
         inputBuffer.inputFrames[inputBuffer.headId%IMG_BUFFER_SIZE].FrameId = inputBuffer.headId;
         inputBuffer.headId++;
+        Cache_wb((xdc_Ptr)&inputBuffer.headId,4,Cache_Type_L1D,1);
+        Cache_wb((xdc_Ptr)&inputBuffer.headId,4,Cache_Type_L2D,1);
 
         #ifdef DEBUG
         debug_imageReceive_time = (float)(Timestamp_get32() - t_start)/1000;
         #endif
-        Notify_sendEvent(1,0,123456,&inputBuffer.inputFrames[inputBuffer.headId%IMG_BUFFER_SIZE].buffer,FALSE);
+        //Cache_wbAll();
 }
