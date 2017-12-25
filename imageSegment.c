0ed5de6f48549ea16b27213a38e303927cb9d742
Victor LI <victor-li@outlook.com>
2017-12-25 23:49:27 +0800

    Modified taskImageSegment - Fatal Error

--- imageSegment.c
+++ imageSegment.c
@@ -8,13 +8,28 @@ Void taskImageSegment(UArg a0)
         unsigned int t_start = Timestamp_get32();
         #endif
         Cache_inv((xdc_Ptr)&(inputBuffer.headId),4,Cache_Type_ALLD,1);
+        Cache_inv((xdc_Ptr)&(thresholdBuffer.tailId),4,Cache_Type_ALLD,1);
         if(inputBuffer.tailId<inputBuffer.headId-1)
-        inputBuffer.tailId++;
-        Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L1D,1);
-        Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L2D,1);
+        {
+
+                unsigned char *p = (inputBuffer.buffer[inputBuffer.tailId%IMG_BUFFER_SIZE].buffer);
+                unsigned char *q = (thresholdBuffer.buffer[thresholdBuffer.headId%IMG_BUFFER_SIZE].buffer);
+                IMG_thr_le2min_8(p,q,IMG_WIDTH,IMG_HEIGHT,IMG_THRES);
+                inputBuffer.tailId++;
+                thresholdBuffer.headId++;
+                Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L1D,1);
+                Cache_wb((xdc_Ptr)&(inputBuffer.tailId),4,Cache_Type_L2D,1);
+                Cache_wb((xdc_Ptr)&(thresholdBuffer.headId),4,Cache_Type_L1D,1);
+                Cache_wb((xdc_Ptr)&(thresholdBuffer.headId),4,Cache_Type_L2D,1);
+        }
+        
 
         #ifdef DEBUG
         debug_imageSegment_time = (float)(Timestamp_get32() - t_start)/1000;
+        Cache_wb((xdc_Ptr)&(debug_imageSegment_time),sizeof(float),Cache_Type_L1D,1);
+        Cache_wb((xdc_Ptr)&(debug_imageSegment_time),sizeof(float),Cache_Type_L2D,1);
         #endif
         
+        Clock_tick();
+
 }
