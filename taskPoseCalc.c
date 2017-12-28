08c961f7b097a62708ad53c2df18dc4b5b433e6e
Victor LI <victor-li@outlook.com>
2017-12-28 19:22:57 +0800

    Add camera calibration matrix.

--- taskPoseCalc.c
+++ taskPoseCalc.c
@@ -1,6 +1,21 @@
 #include <MonoGlobal.h>
 
+float M[3][3] = { 2294.35,      0,  1172.41,
+                        0,2310.24,  885.828,
+                        0,      0,        1};
+float X[4][3] = {       0,      0,        1,
+                       90,      0,        1,
+                      200,      0,        1,
+                        0,     75,        1};
+
 Void taskPoseCalc()
 {
-
+  float Y[4][3];
+  int i=0,j=0;
+  for(i=0;i<4;i++)
+  {
+    Y[i][0] = posBuffer.buffer[i].X;
+    Y[i][1] = posBuffer.buffer[i].Y;
+    Y[i][2] = posBuffer.buffer[i].Z;
+  }
 }
