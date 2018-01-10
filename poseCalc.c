2144c51c97d7e93fe2d0ff48908853458218ef75
Victor LI <victor-li@outlook.com>
2018-01-10 18:56:41 +0800

    Add solveQuarticEquation fucntion. -Solve λ and μ from given Ip^ and Jp^

--- poseCalc.c
+++ poseCalc.c
@@ -55,6 +55,54 @@ void DSPF_sp_crossMat(float *restrict x,float (* restrict y)[3][3])
   (*y)[2][2] = 0;
 }
 
+/**
+ * @brief 
+ * Solve λ and μ from given Ip^ and Jp^
+ * @param IJphap[IN] 4x2 matrix [Ip^,Jp^]
+ * @param lambda[OUT] pointer to 2x1 vector 
+ * @param miu[OUT] pointer to 2x1 vector
+ */
+void solveQuarticEquation(float (* restrict IJphap)[4][2],float *restrict lambda,float *restrict miu)
+{
+  float x0,y0,a,b,c,d,e,g,A,B,C,delta;
+  float err[2];
+  x0 = (*IJphap)[3][0];
+  y0 = (*IJphap)[3][1];
+  a = x0*y0/(1+x0*x0);
+  b = x0*y0/(1+y0*y0);
+  c = (*IJphap)[0][0]*(*IJphap)[0][1] + (*IJphap)[1][0]*(*IJphap)[1][1] + (*IJphap)[2][0]*(*IJphap)[2][1];
+  d = (*IJphap)[0][0]*(*IJphap)[0][0] + (*IJphap)[1][0]*(*IJphap)[1][0] + (*IJphap)[2][0]*(*IJphap)[2][0];
+  e = (*IJphap)[0][1]*(*IJphap)[0][1] + (*IJphap)[1][1]*(*IJphap)[1][1] + (*IJphap)[2][1]*(*IJphap)[2][1];
+  g = (1 + x0*x0)/(1 + y0*y0);
+
+  A = a*a - g;
+  B = 2*a*a*d - g*d + e - 2*a*c;
+  C = a*a*d*d + c*c - 2*a*c*d;
+
+  delta = B*B - 4*A*C;
+  lambda[0] = a>0?(-b+sqrtsp(delta))/(2*a):(-b-sqrtsp(delta))/(2*a);
+  lambda[1] = - lambda[0];
+
+  A = b*b - 1/g;
+  B = 2*b*b*e - e/g + d - 2*b*c;
+  C = b*b*e*e + c*c - 2*b*c*e;
+
+  delta = B*B - 4*A*C;
+  miu[0] = a>0?(-b+sqrtsp(delta))/(2*a):(-b-sqrtsp(delta))/(2*a);
+  miu[1] = - miu[0];
+
+  err[0] = abs(c+lambda[0]*miu[0]-a*(d*+2*lambda[0]*lambda[0])) + abs(c+lambda[0]*miu[0]-b*(e*+2*miu[0]*miu[0])); 
+  err[1] = abs(c+lambda[0]*miu[1]-a*(d*+2*lambda[0]*lambda[0])) + abs(c+lambda[0]*miu[1]-b*(e*+2*miu[0]*miu[0])); 
+
+  if(err[0]>err[1])
+  {
+    delta = miu[0];
+    miu[0] = miu[1];
+    miu[1] = delta;
+  }
+
+}
+
 Void taskPoseCalc()
 {
   
