315fe34b4c43910e10e98ce0c25d9390edfc620c
Victor LI <victor-li@outlook.com>
2018-01-10 19:29:41 +0800

    Modified camera calibration matrix - Remove inv(M) - Resize to 3 x 3 - Update parameters

--- poseCalc.c
+++ poseCalc.c
@@ -1,15 +1,9 @@
 #include <MonoGlobal.h>
 //Camera Calibration Matrix
 #pragma DATA_ALIGN(M, 8)
-float M[4][4] = {2294.35, 0, 1172.41, 0,
-                 0, 2310.24, 885.828, 0,
-                 0, 0,  1,  0,
-                 0, 0,  0,  0};
-#pragma DATA_ALIGN(invM, 8)
-float invM[4][4] = {4.358532917819862e-04, 0, -0.510998757818118, 0,
-                 0, 4.328554609044948e-04, -0.383435487222107, 0,
-                 0, 0,  1,  0,
-                 0, 0,  0,  0};
+float M[3][3] = {2.272727272727273e+03, 0, 640,
+                 0, 2.272727272727273e+03, 640,
+                 0, 0,  1};
 
 //Target Location Matrix
 #pragma DATA_ALIGN(P, 8)
