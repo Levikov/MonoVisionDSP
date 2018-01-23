effeeb60fea29b9f88d496799f6901aa0bdc6c4c
Victor LI <victor-li@outlook.com>
2018-01-23 09:41:34 +0800

    Move configurations to config.h

--- MonoGlobal.h
+++ MonoGlobal.h
@@ -22,23 +22,8 @@
 #include <ti/mathlib/mathlib.h>
 #include <ti/sysbios/BIOS.h>
 #include <ti/sysbios/knl/Task.h>
+#include <config.h>
 
-#define PI 3.14159265358979323846
-
-#define IMG_WIDTH 1280
-#define IMG_HEIGHT 1280
-#define IMG_SIZE 1638400
-#define IMG_THRES 20
-
-#define IMG_BUFFER_SIZE 8
-
-#define TARGET_NUM 4
-
-#define CORE_NUM 8
-#define CLOCK_TICK_TIMEOUT 250000
-
-#define EMIF_IMG 0x7000000;
-#define EMIF_POSE 0x7400000;
 //===========Debug Variables==========//
 #ifdef DEBUG
 extern float debug_imageReceive_time;
