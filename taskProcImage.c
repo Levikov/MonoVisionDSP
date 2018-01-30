68934d3fb2002e4b04455ae72a8e9fb94c5e9b13
Victor LI <victor-li@outlook.com>
2018-01-30 09:17:17 +0800

    Remove debug settings.

--- taskProcImage.c
+++ taskProcImage.c
@@ -9,10 +9,6 @@ void taskProcImage(UArg a0)
         void *buffer_CC;
         int sizeCC;
         
-        while(true)
-        {
-        
-        #ifdef DEBUG_IMG
         // Copy debug image into buffer;
         recvEMIF(emifRecvAddr,image);
 
@@ -31,34 +27,8 @@ void taskProcImage(UArg a0)
         //Pose Calculation
         poseCalc(&points,&pose);
         
-        #else
-        Pose poseTest;
-        poseTest.T.X = 0;
-        poseTest.T.Y = 0;
-        poseTest.T.Z = 800;
-        
-        for(poseTest.R.roll = POSE_CALC_TEST_ROLL_LOW;
-            poseTest.R.roll<= POSE_CALC_TEST_ROLL_HIGH;
-            poseTest.R.roll+= POSE_CALC_TEST_ROLL_DEV)
-        for(poseTest.R.yaw = POSE_CALC_TEST_YAW_LOW;
-            poseTest.R.yaw<= POSE_CALC_TEST_YAW_HIGH;
-            poseTest.R.yaw+= POSE_CALC_TEST_YAW_DEV)
-        for(poseTest.R.pitch = POSE_CALC_TEST_PITCH_LOW;
-            poseTest.R.pitch<= POSE_CALC_TEST_PITCH_HIGH;
-            poseTest.R.pitch+= POSE_CALC_TEST_PITCH_DEV)
-        {
-                generateCoordinates(poseTest,&p);
-                poseCalc(&p,&pose);
-        }
-        #endif
-
         //Send result
         sendEMIF(emifSendAddr,&pose);
 
-        #ifdef DEBUG_IMG
         Memory_free(NULL,buffer_CC,sizeCC);
-        #endif
-
-        };
-
 }
