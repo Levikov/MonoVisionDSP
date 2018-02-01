c4b9dd8c3814e95b2002aeacd2764fbfb39a3ee0
Victor Li <victor-li@outlook.com>
2018-02-02 02:02:27 +0800

    No longer link lapack & blas

--- make.sh
+++ make.sh
@@ -1,2 +1,2 @@
 gcc -g -c -O0 -I/home/victor/Codes/levmar *.c
-gcc -g -O0 -L/home/victor/Codes/levmar -L/home/victor/Codes/CLAPACK *.o -llevmar -llapack -lf2c -lblas -lm
+gcc -g -O0 -L/home/victor/Codes/levmar *.o -llevmar -lm
